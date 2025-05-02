6b319dce6b24a571f2278eb06bb536e3d0e402ff
Omar Jiménez <omarjimenezher@gmail.com>
2025-05-02 09:54:52 -0600

    PANTALLA Y METODOS RESPECTIVOS

--- app/src/main/java/com/example/projectpdm/Pantalla_Menu_administracion.java
+++ app/src/main/java/com/example/projectpdm/Pantalla_Menu_administracion.java
@@ -1,6 +1,7 @@
 package com.example.projectpdm;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class Pantalla_Menu_administracion extends AppCompatActivity {
 
+    MaterialButton btnMateriaPrima, btnMateriales, btnManoObra, btnCostoGastos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,39 @@ public class Pantalla_Menu_administracion extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnMateriaPrima = findViewById(R.id.btn_materia_prima);
+        btnMateriales = findViewById(R.id.btn_materiales);
+        btnManoObra = findViewById(R.id.btn_mano_obra);
+        btnCostoGastos = findViewById(R.id.btn_costos_gastos);
+
+        btnMateriaPrima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnMateriales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnManoObra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCostoGastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
