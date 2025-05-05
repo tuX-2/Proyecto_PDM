f15e9b9f8e7c5d7f1afed24f0f41a0f4813a9df7
GeraSP11 <cortezgerardo662@gmail.com>
2025-05-05 08:40:11 -0600

    Funcionalidad de boton de regreso

--- app/src/main/java/com/example/projectpdm/CalculadoraQF.java
+++ app/src/main/java/com/example/projectpdm/CalculadoraQF.java
@@ -1,6 +1,8 @@
 package com.example.projectpdm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class CalculadoraQF extends AppCompatActivity {
+
+    MaterialButton btnRegresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class CalculadoraQF extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnRegresar = findViewById(R.id.btnRegresar);
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculadoraQF.this, TiposdeQueso.class);
+                startActivity(intent);
+            }
         });
     }
 }
